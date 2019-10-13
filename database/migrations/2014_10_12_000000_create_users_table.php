e151827d94eafacb2a502980f31429d2ecac50b5
Aleksandar Ileiv <aleksandar.iliev@plinga.com>
2019-10-14 01:17:44 +0200

    Added API authentication

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('password', 64);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable()->index();
+            $table->string('api_token', 80)->unique()->nullable()->default(null);
             $table->rememberToken();
             $table->timestamps();
         });
